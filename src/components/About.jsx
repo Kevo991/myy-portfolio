887c39986716691f3842058496634f39b710d5d6
kevo99 <dixonkelvin98@gmail.com>
2023-07-06 14:13:58 +0100

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+      
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+      
+      <div className='pb-8'>
+      
+        <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About </p>
+          </div>
+          <p className=' text-xl mt-20'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+           Sequi veniam nihil dolor! Tempore voluptates aut autem quaerat obcaecati quidem voluptatibus assumenda velit veritatis illo repellat tempora fugiat ad vero necessitatibus, qui adipisci atque rem quia, reprehenderit excepturi iure provident nam possimus? Cupiditate cumque consequatur placeat laudantium harum eum animi quibusdam.</p>
+          <br />
+
+          <p className='text-xl'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+             Amet quos dolor id quam molestias rerum vel porro tenetur iure sint voluptates doloribus pariatur, aut ut architecto ratione? Aliquam numquam ipsam quis fugiat ut nobis provident itaque vero molestias, exercitationem ex harum reprehenderit sequi saepe dicta consequatur repellat esse deleniti qui!
+          </p>
+          </div>
+          </div>
+  )
+}
+
+export default About
